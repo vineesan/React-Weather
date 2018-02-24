9957d92473c295cba929c4e6e4ab055a1d538672
Vineeth <vinith.vini@gmail.com>
2018-02-24 20:35:21 +0530

    LocalHost Server.js Change

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 8080;
 app.use(function(req, res, next) {
-  if(req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if(req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 app.use(express.static('public'));
