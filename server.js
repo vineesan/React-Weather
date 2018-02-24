8a9f9b38ae113ed9002c535e276798b31f1c716a
Vineeth <vinith.vini@gmail.com>
2018-02-24 18:24:50 +0530

    Repo for Heroku

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 8080;
+app.use(function(req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
 app.listen(8080, function() {
   console.log("Server is running in 8080");
