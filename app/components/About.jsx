48f8a00b1acda41ee9c7dbe326d62c16f7284326
Vineeth <vinith.vini@gmail.com>
2018-02-24 20:20:58 +0530

    Content Changes in all pages

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,7 +12,8 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h2>About Component</h2>
+      <h2>About!</h2>
+      <p>Welcome to About page</p>
     </div>
   );
 };
